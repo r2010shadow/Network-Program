08ad765785e6c2e788fad60c3b71ba8da9776359
r2010shadow <r2010shadow@163.com>
2017-09-27 09:02:55 +0800

    done chater1

--- simple_socket/echo_server.py
+++ simple_socket/echo_server.py
@@ -12,22 +12,22 @@ def echo_server(port):
     sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
     server_address = (host, port)
-    print "Starting up echo server on %s port %s " % server_address
+    print ("Starting up echo server on %s port %s " % server_address)
     sock.bind(server_address)
     sock.listen(backlog)
     while True:
-        print "Waiting to receive message from client"
+        print ("Waiting to receive message from client")
         client, address = sock.accept()
         data = client.recv(data_payload)
         if data:
-            print "Data: %s" %data
+            print ("Data: %s" % data)
             client.send(data)
-            print "sent %s bytes back to %s" % (data, address)
+            print ("sent %s bytes back to %s" % (data, address))
         client.close()
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Socket Server Example")
-    parser.add_argument("--port", action="store", dest="port", type=int,required=True)
+    parser.add_argument('--port', action="store", dest="port", type=int,required=True)
     given_args = parser.parse_args()
     port = given_args.port
     echo_server(port)
